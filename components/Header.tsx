ca6e59c2bf89d1edc9c3f00e037a3f16652c3c84
Programmingchuks24 <chuksezeaku@gmail.com>
2024-09-04 15:30:28 +0100

    fixed header err 003

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,24 +6,30 @@ import MainButton from './ui/MainButton';
 
 const Header = () => {
   return (
-    <div className = " text-white flex justify-between px-4 pt-5 items-center">
-
+    <div className=" text-white flex justify-between px-4 pt-5 items-center">
       <div>
-        <Image src = "/company_logo_new.webp" alt = "new image" width = {178} height = {178}/>
+        <Image
+          src="/company_logo_new.webp"
+          alt="new image"
+          width={178}
+          height={178}
+        />
       </div>
 
-      <div className = "hidden md:flex gap-5">
+      <div className="hidden md:flex gap-5">
         <p>Articles</p>
-        <p className = "w-44 h-12 border p-5">Book Free Consultation</p>
+        <p className="w-44 h-12 border p-5">Book Free Consultation</p>
       </div>
 
-      <MenuIcon className = "w-16 h-8 md:hidden lg:hidden"/>
-    
+      <div className= "md:hidden">
+        <MenuIcon className="w-16 h-8" />
+      </div>
+
       {/* <MainButton onClick = {()=>console.log("heey")} className="hidden">
         click me
       </MainButton> */}
     </div>
-  )
+  );
 }
 
 export default Header
