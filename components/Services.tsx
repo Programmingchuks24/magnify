30e3dcb6b3789181733b78d149e3986ba9886dc2
Programmingchuks24 <chuksezeaku@gmail.com>
2024-09-12 11:56:38 +0100

    made changes to services

--- components/Services.tsx
+++ components/Services.tsx
@@ -8,15 +8,15 @@ const Services = () => {
 
     return (
 
-        <div className = "text-white flex flex-col gap-12">
-            <div className = "flex flex-col place-items-center md:place-items-start">
+        <div className = "text-white flex flex-col gap-12 place-items-center">
+            <div className = "flex flex-col place-items-center md:place-self-start md:place-items-start">
                 <p className = "text-blue-300 mb-3">Services</p>
                 <p className = "text-xl">Our Services</p>
                 <p>Tailored To Your Needs</p>
             </div>
            
 
-            <section className = "grid grid-cols-2 gap-x-3  items-center gap-y-20  text-sm md:grid-cols-3 md:gap-x-16 justify-items-center">
+            <section className = "grid grid-cols-2 gap-x-16 gap-y-20  text-sm md:grid-cols-3 md:gap-x-[20rem]">
 
                 {section.map((item)=> (
 
@@ -30,6 +30,8 @@ const Services = () => {
 
                 ))}
             </section>
+
+            <p className = "text-blue-300">and more...</p>
         </div>
 
     );
